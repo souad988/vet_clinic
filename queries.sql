97e53aee8dcf4e81d0a88bb79854e619ed8f2a7a
souad988 <59707859+souad988@users.noreply.github.com>
2022-04-26 21:52:45 +0000

    Query and update animals table

--- queries.sql
+++ queries.sql
@@ -15,3 +15,17 @@ SELECT * FROM animals WHERE neutered IS TRUE;
 SELECT * FROM animals WHERE name<>'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+
+--How many animals are there?
+SELECT count(*) FROM animals;
+--How many animals have never tried to escape?
+SELECT count(*) FROM animals WHERE escape_attempts > 0;
+--What is the average weight of animals?
+SELECT AVG(weight_kg) FROM animals;
+--Who escapes the most, neutered or not neutered animals?
+SELECT neutered,AVG(escape_attempts) FROM animals GROUP BY neutered;
+--What is the minimum and maximum weight of each type of animal?
+SELECT species,min(weight_kg),max(weight_kg) FROM animals GROUP BY species;
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT species,AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-01-31' GROUP BY species ; 

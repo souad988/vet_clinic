789ceef9477ea5c517be1512ed26807c173ee76d
souad988 <59707859+souad988@users.noreply.github.com>
2022-04-29 13:04:16 +0000

    Update animals to include owner and species  foreign key

--- schema.sql
+++ schema.sql
@@ -10,3 +10,22 @@ CREATE TABLE animals(
 );
 
 ALTER TABLE animals ADD species CHAR(100);
+
+CREATE TABLE owners(
+   id  SERIAL PRIMARY KEY     NOT NULL,
+   full_name           TEXT    NOT NULL,
+   age  NUMERIC
+);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_species
+      FOREIGN KEY(species_id) 
+	  REFERENCES species(id)
+	  ON DELETE CASCADE;
+
+ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals ADD CONSTRAINT fk_owner
+      FOREIGN KEY(owner_id) 
+	  REFERENCES owners(id)
+	  ON DELETE CASCADE;     

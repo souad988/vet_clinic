63a224b5463493aa06859390769311ab916ca909
Souad El Mansouri <souadelmansouri88@gmail.com>
2022-04-30 00:07:15 +0000

    Change id and age to be of type INT

--- schema.sql
+++ schema.sql
@@ -1,30 +1,30 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE TABLE animals(
-   id SERIAL PRIMARY KEY     NOT NULL,
-   name           TEXT    NOT NULL,
+   id INT GENERATED ALWAYS AS IDENTITY,
+   name           VARCHAR(250),
    date_of_birth            date     NOT NULL,
    escape_attempts      INT NOT NULL,	
    neutered        BOOLEAN NOT NULL,
    weight_kg         REAL
 );
 
-ALTER TABLE animals ADD species CHAR(100);
+ALTER TABLE animals ADD species VARCHAR(250);
 
 CREATE TABLE owners(
-   id  SERIAL PRIMARY KEY     NOT NULL,
-   full_name           TEXT    NOT NULL,
-   age  NUMERIC
+   id INT GENERATED ALWAYS AS IDENTITY,
+   full_name       VARCHAR(250),
+   age  INT
 );
 
 ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD COLUMN species_id INTEGER;
+ALTER TABLE animals ADD COLUMN species_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_species
       FOREIGN KEY(species_id) 
 	  REFERENCES species(id)
 	  ON DELETE CASCADE;
 
-ALTER TABLE animals ADD COLUMN owner_id INTEGER;
+ALTER TABLE animals ADD COLUMN owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_owner
       FOREIGN KEY(owner_id) 
 	  REFERENCES owners(id)

a0a651f7a1269c173a396386431651eaf98cc492
souad988 <59707859+souad988@users.noreply.github.com>
2022-04-30 10:51:23 +0000

    Create speciaizations table and Create queries to select from joined tables

--- schema.sql
+++ schema.sql
@@ -29,3 +29,30 @@ ALTER TABLE animals ADD CONSTRAINT fk_owner
       FOREIGN KEY(owner_id) 
 	  REFERENCES owners(id)
 	  ON DELETE CASCADE;     
+
+-- add "join table" for visits
+
+CREATE TABLE vets(
+id SERIAL PRIMARY KEY     NOT NULL,
+name TEXT,
+age NUMERIC,
+date_of_graduation date
+)
+
+CREATE TABLE specializations(
+    species_id INT,
+    vets_id INT,
+    PRIMARY KEY(species_id, vets_id),
+    CONSTRAINT species_fk FOREIGN KEY (species_id) REFERENCES species(id),
+    CONSTRAINT vets_fk FOREIGN KEY (vets_id) REFERENCES vets(id)
+  );
+
+CREATE TABLE visits(
+    animals_id INT,
+    vets_id INT,
+    id  SERIAL PRIMARY KEY     NOT NULL,
+    date_of_visit date,
+    CONSTRAINT animals_fk FOREIGN KEY (animals_id) REFERENCES animals(id),
+    CONSTRAINT vets_fk FOREIGN KEY (vets_id) REFERENCES vets(id)
+  );
+
